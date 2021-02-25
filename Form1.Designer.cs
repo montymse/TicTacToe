068c5cbe3cb88a5f6eb454e87c405c17d05da45b
montymse <mse450@hotmail.com>
2021-02-25 17:06:52 +0100

    TicTacToe

---
+++ Form1.Designer.cs
@@ -0,0 +1,32 @@
+﻿
+namespace TicTacToe
+{
+    partial class TicTacToe
+    {
+      
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        private System.Windows.Forms.Button A1;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.Button A3;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button C3;
+        private System.Windows.Forms.Button C2;
+        private System.Windows.Forms.Button C1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton Reset;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class TicTacToe : Form
+    {
+        enum states { Win, Draw, None }
+
+        Boolean turn = true;
+        int counter = 0;
+        states result = states.None;
+
+        public TicTacToe()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonClick(object sender, EventArgs e)
+        {
+            try
+            {
+                Button b = (Button)sender;
+                b.Text = turn ? "X" : "O";
+                b.Enabled = false;
+
+                turn = !turn;
+                counter++;
+
+                checkgame();
+
+            } catch {}           
+        }
+
+        private void checkgame()
+
+
+        {
+            if (counter==9) result = states.Draw;
+
+            if ((A1.Text == A2.Text) && (A2.Text == A3.Text) && !A3.Enabled) result = states.Win;
+            if ((B1.Text == B2.Text) && (B2.Text == B3.Text) && !B3.Enabled) result = states.Win;
+            if ((C1.Text == C2.Text) && (C2.Text == C3.Text) && !C3.Enabled) result = states.Win;
+
+            if ((A1.Text == B1.Text) && (B1.Text == C1.Text) && !C1.Enabled) result = states.Win;
+            if ((A2.Text == B2.Text) && (B2.Text == C2.Text) && !C2.Enabled) result = states.Win;
+            if ((A3.Text == B3.Text) && (B3.Text == C3.Text) && !C3.Enabled) result = states.Win;
+
+            if ((A1.Text == B2.Text) && (B2.Text == C3.Text) && !C3.Enabled) result = states.Win;
+            if ((A3.Text == B2.Text) && (B2.Text == C1.Text) && !C1.Enabled) result = states.Win;
+
+            if (result==states.Draw) { MessageBox.Show("Draw"); }
+            if (result==states.Win)
+            {
+                disableGame();
+                string winner = turn ? "O" : "X";
+                MessageBox.Show(winner + " wins!");
+            }
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            turn = true;
+            counter = 0;
+            result = states.None;
+
+            foreach (Control c in Controls)
+            {
+                try
+                {
+                    Button b = (Button)c;
+                    b.Enabled = true;
+                    b.Text = "";
+                }
+                catch { }
+
+            }
+        }
+
+        private void disableGame()
+        {
+            foreach (Control c in Controls)
+            {
+                try
+                {
+                    Button b = (Button)c;
+                    b.Enabled = false;
+                }
+                catch {}
+
+            }
+        }
+
+        private void mouseEnter(object sender, EventArgs e)
+        {
+                Button b = (Button)sender;
+                if (b.Enabled)
+                {
+                    b.Text = turn ? "X" : "O";
+                }
+        }
+
+        private void mouseLeave(object sender, EventArgs e)
+        {
+                Button b = (Button)sender;
+                if (b.Enabled)
+                {
+                    b.Text = "";
+                }         
+        }
+
+    }
+}
